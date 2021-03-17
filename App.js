12d8e27247c6048ab3dce1c7ebbc515f8c1bd744
Gauthier IL <gauthier-il@hotmail.fr>
2021-03-17 09:14:03 +0100

    Add Bottom Nav Bar

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Search from './Components/Search'
+import MyTabs from './Views/BottomTabNav'
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello ! Ca va ?</Text>
-
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
