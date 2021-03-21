c7b77fb61bf06d86b19e1f664d93be9d86a237e0
Gauthier IL <gauthier-il@hotmail.fr>
2021-03-21 01:14:44 +0100

    Start application with page of sign up

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import BottomTabNav from './Views/BottomTabNav';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
+import NavigationLogSign from './Navigation/NavigationLogSign';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    'ScopeOne' : require('./assets/fonts/Scope_One/ScopeOne.ttf'),
-    'DancingScript' : require('./assets/fonts/Dancing_Script/DancingScript.ttf'),
-    'OpenSans' : require('./assets/fonts/Open_Sans/OpenSans-Regular.ttf'),
-    'OpenSansLight' : require('./assets/fonts/Open_Sans/OpenSans-Light.ttf'),
-    'OpenSansBold' : require('./assets/fonts/Open_Sans/OpenSans-Bold.ttf'),
-    'OpenSansItalic' : require('./assets/fonts/Open_Sans/OpenSans-Italic.ttf'),
+    'ScopeOne' : require('./Assets/fonts/Scope_One/ScopeOne.ttf'),
+    'DancingScript' : require('./Assets/fonts/Dancing_Script/DancingScript.ttf'),
+    'OpenSans' : require('./Assets/fonts/Open_Sans/OpenSans-Regular.ttf'),
+    'OpenSansLight' : require('./Assets/fonts/Open_Sans/OpenSans-Light.ttf'),
+    'OpenSansBold' : require('./Assets/fonts/Open_Sans/OpenSans-Bold.ttf'),
+    'OpenSansItalic' : require('./Assets/fonts/Open_Sans/OpenSans-Italic.ttf'),
 
   });
 
@@ -18,5 +18,5 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <BottomTabNav/>;
+  return <NavigationLogSign/>;
 }
