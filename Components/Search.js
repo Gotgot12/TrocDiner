2d9e238d2700908df8c7fe07a35e522c6e580023
Gauthier IL <gauthier-il@hotmail.fr>
2021-03-21 01:17:38 +0100

    Css change + add stylesheet

--- Components/Search.js
+++ Components/Search.js
@@ -1,12 +1,14 @@
 // Components/Search.js
 
 import React from 'react'
-import { StyleSheet, View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput } from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons";
 
 class Search extends React.Component {
     render() {
         return (
             <View style={styles.main_container}>
+                <Icon name="search-outline" size={15} color="#008037"/>
                 <TextInput style={styles.textinput} placeholder="Recherche de plat"/>
             </View>
         );
@@ -16,16 +18,20 @@ class Search extends React.Component {
 const styles = StyleSheet.create({
     main_container: {
         flex: 1,
-        marginTop: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
     },
 
     textinput: {
-        marginLeft: 5, 
-        marginRight: 5, 
-        height: 50, 
+        height: 30,
+        width: 200,
         borderColor: '#000000',
         borderWidth: 1, 
-        paddingLeft: 5
+        paddingLeft: 10,
+        borderRadius: 10,
+        borderColor: '#008037',
+        marginLeft: 5,
     }
 })
 
--- Components/TopHeader.js
+++ Components/TopHeader.js
@@ -1,27 +1,58 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 class TopHeader extends React.Component {
-    render() {
-        return (
-            <View style={{flex: 1, flexDirection: 'row', marginTop: 20, paddingBottom: 10, alignItems: 'flex-end', backgroundColor: '#008037'}}>
-                <View style={{flex: 1, alignItems: 'flex-start'}}>
-                    <Icon name="page-layout-sidebar-left" size={30} color='white'/>
-                </View>
-                <View style={{flex: 2, alignItems: 'center'}}>
-                    <Text style={{fontSize: 25, fontFamily: 'OpenSansItalic', color: 'white'}}>Troc Diner</Text>
-                </View>
-                <View style={{flex: 1, alignItems: 'flex-end'}}>
-                    <Icon name="account" size={30} color='white'/>
-                </View>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <View style={styles.iconSidebar_container}>
+          <Icon name="page-layout-sidebar-left" size={35} color="white" />
+        </View>
+        <View style={styles.titre_container}>
+          <Text style={styles.titre_text}>
+            Troc Dîner
+          </Text>
+        </View>
+        <View style={styles.iconProfile_container}>
+          <Icon name="account" size={35} color="white" />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
+    main_container: {
+        flex: 1,
+        flexDirection: "row",
+        marginTop: 20,
+        paddingBottom: 10,
+        alignItems: "center",
+        backgroundColor: "#008037",
+    },
 
-})
+    iconSidebar_container: {
+        flex: 1, 
+        alignItems: "flex-start",
+    },
 
-export default TopHeader
+    titre_container: {
+        flex: 2, 
+        alignItems: "center",
+    },
+
+    titre_text: {
+        fontSize: 40,
+        fontFamily: "DancingScript",
+        color: "white",
+    },
+
+    iconProfile_container: {
+        flex: 1, 
+        alignItems: "flex-end",
+    }
+
+});
+
+export default TopHeader;
