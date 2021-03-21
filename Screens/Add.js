19d6510d2ce320825de1f39afe8fdcbaf94e1655
Gauthier IL <gauthier-il@hotmail.fr>
2021-03-21 01:16:03 +0100

    css stylesheet

--- Screens/Add.js
+++ Screens/Add.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import TopHeader from '../Components/TopHeader';
 
 function Add() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={styles.main_container}>
         <TopHeader/>
         <View style={{ flex: 8, justifyContent: 'center', alignItems: 'center' }}>
           <Text>Add !</Text>
@@ -12,5 +12,13 @@ function Add() {
       </View>
     );
   }
+
+const styles = StyleSheet.create({
+  main_container: { 
+    flex: 1,   
+  },
+
+
+});
 
 export default Add
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import TopHeader from '../Components/TopHeader';
-import Search from '../Components/Search'
+import Search from '../Components/Search';
+import Token from '../Components/Token';
 
 function Home() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={styles.main_container}>
         <TopHeader/>
+        <Token/>
         <Search/>
         <View style={{ flex: 8, justifyContent: 'center', alignItems: 'center' }}>
           <Text>Home !</Text>
@@ -15,4 +17,11 @@ function Home() {
     );
   }
 
+const styles = StyleSheet.create({
+  main_container: { 
+    flex: 1,   
+  },
+  
+  
+});
 export default Home
